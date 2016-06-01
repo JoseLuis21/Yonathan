c18ea9036e742b2606a81e2339c8343fd5bbfeee
JoseLuis21 <josephluihs@gmail.com>
2016-05-31 21:39:19 -0400

    Configuracion del correo y validaciones en español

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,5 @@
-@extends('layouts.app')
+@extends('layouts.principal')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Menu principal</div>
-
-                <div class="panel-body">
-                    Bienvenido
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    <h1>Sistema de control de Ovejas</h1>
 @endsection
